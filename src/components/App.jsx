924a8f8936a4dd8e4928889c2868f1fdc8fc4da6
Mykkolla <tarabukin.n.10@gmail.com>
2023-05-28 15:50:32 +0300

    A

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,14 @@ import ContactList from './ContactList/ContactList';
 import { Layout } from './Style/Layout';
 import { useState, useEffect } from 'react';
 
-export default function PhoneBook() {
-  const initialContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
+export default function PhoneBook() {
   const [contacts, setContacts] = useState(
     JSON.parse(localStorage.getItem('contacts')) ?? initialContacts
   );
